b4b57001e9026743fc8cc2729967d63fe0ac958a
bulg0re <eric.mabed@gmail.com>
2016-12-13 11:38:36 +0100

    Modification de login.php: Traitement du formulaire de connexion

--- login.php
+++ login.php
@@ -1,1 +1,16 @@
-<?php /*//Traitement du formulaire//*/ ?>
+<?php 
+/*
+Traitement du formulaire
+*/ 
+if(!empty($_POST["identifiant"]) && !empty($_POST["pass"]))
+{
+	$identifiant 	= htmlspecialchars(trim($_POST["identifiant"]));
+	$password 		= htmlspecialchars(trim($_POST["pass"]));
+	if($identifiant == "Bulg0re" && $password == "github")
+		echo "Bienvenue!!!";
+	else
+		echo "Dommage..."
+}
+else
+	echo "Partisan du moindre effort...";
+?>

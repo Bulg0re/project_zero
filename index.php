a39007ef16cba8f0a82c25a1f946326b42bffdf7
bulg0re <eric.mabed@gmail.com>
2016-12-13 11:28:22 +0100

    Modification de login.php: ajout du formulaire

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php //Formulaire html5 ?>
+<?php 
+//Formulaire html5 
+?>
+<!Doctype>
+<html>
+	<head></head>
+	<body>	
+		<section>
+			<form action="login.php" method="POST">
+				<label for="identifiant">Votre identifiant:</label><br/>
+				<input type="text" name="identifiant" /><br/>
+				<label for="pass">Votre mot de passe:</label><br/>
+				<input type="password" name="pass" /><br/>
+				<input type="submit" value="Valider" />
+			</form>
+		</section>
+	</body>
+</html>
